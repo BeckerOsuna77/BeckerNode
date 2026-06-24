5affe9143fc3e127e0c4790a3b72585cf82a08ef
BeckerOsuna77 <BeckerOsuna77>
2026-06-23 19:49:04 -0700

    agregando respuesta de un usuario

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('Servidor corriendo en http://localhost:3000');
 });
+
+app.get('/usuario', (req, res) => {
+  const usuario = {
+    id: 1,
+    nombre: 'Juan',
+    rol: 'Administrador'
+  };
+
+  res.json(usuario);
+});
