5f0518956a4e3682c8fb3c9a466b19c527486813
BeckerOsuna77 <BeckerOsuna77>
2026-06-23 20:32:43 -0700

    probando la base de datos

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
 const express = require('express');
-const app = express();
 const pool = require('./db');
+const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hola mundo desde Express');
+  res.send('API funcionando');
+});
+
+app.get('/alumnos', async (req, res) => {
+  try {
+    const resultado = await pool.query('SELECT * FROM alumno');
+    res.json(resultado.rows);
+  } catch (error) {
+    console.error('Error al consultar alumnos:', error);
+    res.status(500).json({ error: 'Error al obtener los alumnos' });
+  }
 });
 
 app.listen(3000, () => {
   console.log('Servidor corriendo en http://localhost:3000');
 });
-
-app.get('/usuario', (req, res) => {
-  const usuario = {
-    id: 1,
-    nombre: 'Juan',
-    rol: 'Administrador'
-  };
-
-  res.json(usuario);
-});
-
-pool.connect()
-  .then(() => {
-    console.log('Conexión exitosa a PostgreSQL');
-  })
-  .catch((err) => {
-    console.error('Error de conexión', err);
-  });
